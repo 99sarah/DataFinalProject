7b621212ade7d97d98c54bbba779d0a492fec15e
johanneshefe <johannes.helle-feldmann@stud.hs-hannover.de>
2023-10-30 15:43:31 +0100

    added gitignore & reformatted app.py

--- app.py
+++ app.py
@@ -7,18 +7,20 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
+
 
 @callback(
     Output('graph-content', 'figure'),
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
+    dff = df[df.country == value]
     return px.line(dff, x='year', y='pop')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
