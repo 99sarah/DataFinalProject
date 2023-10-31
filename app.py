2cb0b049c9a04e3f7a8c58a5a0f91b86fcf95c9c
sarahhaunhorst <sarah.haunhorst@stud.hs-hannover.de>
2023-10-31 16:09:18 +0100

    first interactive app with dash

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
+import plotly.express as px
+import dash_mantine_components as dmc
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-app = Dash(__name__)
+# Initialize the app - incorporate a Dash Mantine theme
+external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-])
+# App layout
+app.layout = dmc.Container([
+    dmc.Title('My First App with Data, Graph, and Controls', color="blue", size="h3"),
+    dmc.RadioGroup(
+            [dmc.Radio(i, value=i) for i in  ['pop', 'lifeExp', 'gdpPercap']],
+            id='my-dmc-radio-item',
+            value='lifeExp',
+            size="sm"
+        ),
+    dmc.Grid([
+        dmc.Col([
+            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
+        ], span=6),
+        dmc.Col([
+            dcc.Graph(figure={}, id='graph-placeholder')
+        ], span=6),
+    ]),
 
+], fluid=True)
 
+# Add controls to build the interaction
 @callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Output(component_id='graph-placeholder', component_property='figure'),
+    Input(component_id='my-dmc-radio-item', component_property='value')
 )
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
-
+# Run the App
 if __name__ == '__main__':
     app.run(debug=True)
