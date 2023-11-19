9c0213eb089aebb5851e1eac981f0d8969cea62a
johanneshefe <johannes.helle-feldmann@stud.hs-hannover.de>
2023-11-19 14:34:48 +0100

    updated csv

--- data/covidData.py
+++ data/covidData.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
-kCovidDf = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
+kCovidDf = pd.read_csv('../data/owid-covid-data.csv')
+kResponseTrackerDf = pd.read_csv('../data/OxCGRT_compact_national_v1.csv', parse_dates=['Date'])
