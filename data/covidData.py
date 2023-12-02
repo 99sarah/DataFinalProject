b778268a811f499fc61d00d710da08e7fd0418c4
johanneshefe <johannes.helle-feldmann@stud.hs-hannover.de>
2023-12-02 19:51:05 +0100

    added lookup csv for ordinal data

--- data/covidData.py
+++ data/covidData.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 kCovidDf = pd.read_csv('../data/owid-covid-data.csv', parse_dates=['date'])
 kResponseTrackerDf = pd.read_csv('../data/OxCGRT_compact_national_v1.csv', parse_dates=['Date'])
+kResponseOrdinalMeaning = pd.read_csv('../data/OxCGRT_ordinal_data_meaning.csv', delimiter=';')
 k_iso_code_country_name_df = kResponseTrackerDf[['CountryName', 'CountryCode']].drop_duplicates()
 k_iso_to_name = k_iso_code_country_name_df.set_index('CountryCode').to_dict()['CountryName']
 k_name_to_iso = k_iso_code_country_name_df.set_index('CountryName').to_dict()['CountryCode']
