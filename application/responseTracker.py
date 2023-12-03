330461744e34685daba1f359a775314e7cee0c09
johanneshefe <johannes.helle-feldmann@stud.hs-hannover.de>
2023-12-03 16:35:25 +0100

    added subplots, readable metric names for responses

--- application/responseTracker.py
+++ application/responseTracker.py
@@ -1,4 +1,5 @@
 import numpy as np
+import plotly.graph_objects
 
 from data.covidData import kCovidDf, kResponseTrackerDf, kResponseOrdinalMeaning
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
@@ -7,6 +8,9 @@ import pandas as pd
 import plotly.graph_objects as go
 from data.covidData import kCovidDf
 from dash.exceptions import PreventUpdate
+from plotly.subplots import make_subplots
+
+k_line_color_palett = ['rgb(50,76,113)', 'rgb(183,30,29)', 'rgb(0,189,139)', 'rgb(236,157,14)', 'rgb(110,175,245)']
 
 responseTrackerTab = dcc.Tab(
     label='Response Tracker',
@@ -25,7 +29,6 @@ responseTrackerTab = dcc.Tab(
                 value='new_cases_smoothed_per_million',
                 className='dbc'
             ),
-            dcc.Graph(id='covid-trend-chart'),
             dcc.Dropdown(
                 kResponseOrdinalMeaning[['Name', 'Description']].rename(
                     columns={'Name': 'value', 'Description': 'label'}).to_dict('records'),
@@ -33,58 +36,64 @@ responseTrackerTab = dcc.Tab(
                 value='C1M_School closing',
                 className='dbc'
             ),
-            dcc.Graph(id='response-chart'),
-            html.Div(id='my-output-d')
+            html.Div(id='my-output-d'),
+            dcc.Graph(id="sub-plots")
         ])
     ])
 
-
-@callback(
-    [Output('covid-trend-chart', 'figure'),
-     Output('response-chart', 'figure')],
-    [Input('country-selection', 'value'),
-     Input('covid-trend-selection', 'value'),
-     Input('metric-selection', 'value')]
-)
-def update_graph(country_names, covid_trend_metric, metric_name):
-    print(f'{country_names}, {covid_trend_metric}, {metric_name}')
-    startDate = np.datetime64('2020-03-01')
-    endDate = np.datetime64('2022-09-01')
-    covid_trend_chart = px.line(
-        kCovidDf.query("location in @country_names and @startDate < date < @endDate"),
-        color='location',
-        x='date',
-        y=covid_trend_metric
-    ).update_xaxes(
-        showspikes=True,
-        spikecolor="white",
-        spikesnap="cursor",
-        spikemode="across",
-        spikedash="solid",
-    )  # .update_traces(xaxis="x4")
-    response_chart = px.line(
-        kResponseTrackerDf.query("CountryName in @country_names and @startDate < Date < @endDate"),
-        color='CountryName',
-        x='Date',
-        y=metric_name
-    ).update_xaxes(
-        showspikes=True,
-        spikecolor="white",
-        spikesnap="cursor",
-        spikemode="across",
-        spikedash="solid",
-    )  # .update_traces(xaxis="x4")
-
-    return covid_trend_chart, response_chart
-
-
 @callback(
     Output('my-output-d', 'children'),
-    Input('covid-trend-chart', 'hoverData'),
+    Input('sub-plots', 'hoverData'),
 )
 def cross_filtering(hover_data):
     if not hover_data:
         raise PreventUpdate
     date = hover_data['points'][0]['x']
     return f'Output: {date}'
+
+
 # TODO Spikeline across both graphs
+
+@callback(
+    Output('sub-plots', 'figure'),
+
+    [Input('country-selection', 'value'),
+     Input('covid-trend-selection', 'value'),
+     Input('metric-selection', 'value')]
+)
+def update_graph(country_names, covid_trend_metric, response_metric_name):
+    startDate = np.datetime64('2020-03-01')
+    endDate = np.datetime64('2022-09-01')
+    nested_plot = make_subplots(rows=2, cols=1, shared_xaxes=True)
+
+    covid_pre_filter = kCovidDf.query("location in @country_names and @startDate < date < @endDate")
+    response_pre_filter = kResponseTrackerDf.query("CountryName in @country_names and @startDate < Date < @endDate")
+    i = 0
+    for country in country_names:
+        covid_country = (covid_pre_filter.query('location == @country')),
+        covid_graph = go.Scatter(
+            x=covid_country[0]['date'],
+            y=covid_country[0][covid_trend_metric],
+            legendgroup=country,
+            name=country,
+            mode='lines',
+            marker=dict(color=k_line_color_palett[i % (len(k_line_color_palett))])
+        )
+        nested_plot.add_trace(covid_graph, row=1, col=1)
+
+        response_country = response_pre_filter.query('CountryName == @country')
+        response_graph =go.Scatter(
+            x=response_country['Date'],
+            y=response_country[response_metric_name],
+            legendgroup=country,
+            name=country,
+            marker=dict(color=k_line_color_palett[i % (len(k_line_color_palett))]),
+            showlegend=False,
+        )
+        response_graph.update(dy=1)
+        nested_plot.add_trace(response_graph, row=2, col=1)
+        i += 1
+    nested_plot.update_xaxes(showspikes=True, spikemode="across")
+    nested_plot.update_traces(xaxis="x2")
+    nested_plot.update_layout(hovermode="x")
+    return nested_plot
